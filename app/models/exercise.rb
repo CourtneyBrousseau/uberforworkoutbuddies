1ff2b12fca3f360e0c944a92352962e55c4881fe
Courtney Brousseau <courtney.brousseau@berkeley.edu>
2018-11-20 12:13:21 -0800

    New workout form now lets you select from list of exercises/gyms

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,2 +1,4 @@
 class Exercise < ApplicationRecord
+	# This is required because you can't technically name an attribute of a model "type" but this fixes it 
+	self.inheritance_column = :_type_disabled
 end
