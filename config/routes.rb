c7a1c0fd9b4ad11438cd19ce99a56376cd3637ad
Courtney Brousseau <courtney.brousseau@berkeley.edu>
2018-11-20 22:25:31 -0800

    Updated root to view of workouts

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :gyms
   resources :exercises
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "workouts#index"
 end
