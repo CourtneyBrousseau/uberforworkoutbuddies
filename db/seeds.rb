571acde695be48c92c5a5db1efc4dd8f97df2c64
Courtney Brousseau <courtney.brousseau@berkeley.edu>
2018-11-27 19:40:01 -0800

    Added first db sb seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Gym.create(name: "RSF", address: "2301 Bancroft Way, Berkeley, CA 94720")
